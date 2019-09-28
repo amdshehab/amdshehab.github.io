321a73e64bc9429f2f06b46d5fc903b5b38bb3be
Shehab <shehab.ahmed.uk@gmail.com>
2019-09-28 18:37:49 +0100

    adds basic structure for tablet and mobile breakpoints, adds scss plugin and removes obselete component

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,17 +1,10 @@
 <script>
-  import FixedSide from "./fixed-side.svelte";
   import Slide from "./slide.svelte";
   export let name;
 </script>
 
-<style>
-  div {
-    display: flex;
-    flex-flow: row wrap;
-  }
-</style>
-
 <div>
-  <FixedSide />
+  <Slide type="fixed" />
+  <Slide />
   <Slide />
 </div>
--- website/src/slide.svelte
+++ website/src/slide.svelte
@@ -1,19 +1,47 @@
 <script>
   export let content;
+  export let type;
 </script>
 
-<style>
+<style type="text/scss">
+  $color: pink;
+
   div {
+    box-sizing: border-box;
+    padding: 100px;
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+
+  .fixed {
     min-height: 100vh;
-    background: green;
-    margin-left: 40vw;
-    width: 60vw;
+    @media screen and (min-width: 1200px) {
+      position: fixed;
+      width: 40vw;
+    }
+  }
+
+  .slider {
+    min-height: 80vh;
+    background-color: $color;
+    @media screen and (min-width: 1200px) {
+      margin-left: 40vw;
+    }
   }
 </style>
 
 <section>
-  <div>
-    <h2>A SLIDE</h2>
-    <p>{content}</p>
+  <div class={type === 'fixed' ? 'fixed' : 'slider'}>
+    <!-- <div class="content"> -->
+    <h1>
+      “Design is not how it looks like and feels like. Design is how it works”
+    </h1>
+    <p>
+      This quote from the legendary Steve Jobs sums up what i do, building
+      things that work. I focus on creating systems
+    </p>
+    <!-- </div> -->
   </div>
 </section>
