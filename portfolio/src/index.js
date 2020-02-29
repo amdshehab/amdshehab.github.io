754e406127ab0e2eaafe45d960c0ed7a92c039a0
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2020-02-29 14:40:55 +0000

    some reorganisation

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as THREE from "three";
 import "./index.css";
-import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 2;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
+
+// const element = document.createElement("div");
+// element.innerHTML = "hellloooooo";
+// document.body.appendChild(element);
